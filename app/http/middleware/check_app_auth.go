f1ffd7b3f3ba93e9fd36968dc2090421f139eab6
seakee <seakee23@gmail.com>
2024-07-24 15:03:25 +0800

    Remove unnecessary package comments.

--- app/http/middleware/check_app_auth.go
+++ app/http/middleware/check_app_auth.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-// Package middleware provides HTTP middleware functions for the application.
 package middleware
 
 import (
