ca2770a63a693db6276e94533289fcf2a27cd1c4
seakee <seakee23@gmail.com>
2025-08-25 13:16:31 +0800

    refactor(router): update handler registration and imports

--- app/http/router/external/service/handler.go
+++ app/http/router/external/service/handler.go
@@ -3,14 +3,10 @@ package service
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/seakee/go-api/app/http"
-	"github.com/seakee/go-api/app/http/router/external/service/auth"
 )
 
 func RegisterRoutes(api *gin.RouterGroup, ctx *http.Context) {
 	api.GET("ping", func(c *gin.Context) {
 		ctx.I18n.JSON(c, 0, nil, nil)
 	})
-
-	authAPI := api.Group("auth")
-	auth.RegisterRoutes(authAPI, ctx)
 }
--- app/http/router/internal/handler.go
+++ app/http/router/internal/handler.go
@@ -3,10 +3,14 @@ package internal
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/seakee/go-api/app/http"
+	"github.com/seakee/go-api/app/http/router/internal/service"
 )
 
 func RegisterRoutes(api *gin.RouterGroup, ctx *http.Context) {
 	api.GET("ping", func(c *gin.Context) {
 		ctx.I18n.JSON(c, 0, nil, nil)
 	})
+
+	serviceAPI := api.Group("service")
+	service.RegisterRoutes(serviceAPI, ctx)
 }
