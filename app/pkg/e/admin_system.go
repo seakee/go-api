05794dcf3e320670f725ab92afb0f75dbccda002
seakee <seakee23@gmail.com>
2026-01-28 21:05:05 +0800

    ✨ feat(e): add admin system error codes

---
+++ app/pkg/e/admin_system.go
@@ -0,0 +1,47 @@
+package e
+
+const (
+	AccountOrPasswordCanNotBeNull  = 11000
+	AccountOrPasswordFail          = 11001
+	AccountNotFound                = 11002
+	FeishuCodeCanNotBeNull         = 11003
+	FeishuAuthFail                 = 11004
+	AccountAuthorizationExpired    = 11005
+	AccountUnauthorized            = 11006
+	InvalidAccount                 = 11007
+	AccountInsufficientPermissions = 11008
+	TokenSignatureInvalid          = 11009
+	InvalidGrantType               = 11010
+	FeishuAccountIsResigned        = 11011
+	FeishuAccountNotExists         = 11012
+	AccountExists                  = 11013
+	AccountCantBeNull              = 11014
+	NeedResetPWD                   = 11015
+	RoleNotFound                   = 11016
+	RoleNameCantBeNull             = 11017
+	RoleNameExists                 = 11018
+	PermissionNotFound             = 11019
+	PermissionNameCantBeNull       = 11020
+	PermissionNameExists           = 11021
+	PermissionHashCantBeNull       = 11022
+	MenuNameExists                 = 11023
+	MenuNotFound                   = 11024
+	MenuHasSubMenu                 = 11025
+	TfaGenQrCodeFail               = 11026
+	TfaVerifyFail                  = 11027
+	NeedTfa                        = 11028
+	InvalidTotpCode                = 11029
+	InvalidSafeCode                = 11030
+	InvalidSToken                  = 11031
+	PasswordCanNotBeNull           = 11032
+	TotpCodeCanNotBeNull           = 11033
+	SafeCodeCanNotBeNull           = 11034
+	TotpKeyCanNotBeNull            = 11035
+	InvalidUserName                = 11036
+	UserCanNotBeOperated           = 11037
+	RoleCanNotBeOperated           = 11038
+	InvalidIPOrCIDR                = 11039
+	OauthTypeNotSupport            = 11040
+	InvalidOauthState              = 11041
+	DockmonLogNotFound             = 11042
+)
