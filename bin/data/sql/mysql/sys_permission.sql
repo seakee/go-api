f60f2f8a1426ac61f71c9d05bfc9c795ff042e1f
seakee <seakee23@gmail.com>
2026-02-02 12:16:03 +0800

    chore(sql): move mysql seed files under bin/data/sql/mysql

--- bin/data/sql/mysql/sys_permission.sql
+++ bin/data/sql/mysql/sys_permission.sql
@@ -2,11 +2,11 @@ CREATE TABLE `sys_permission`
 (
     `id`          int unsigned NOT NULL AUTO_INCREMENT,
     `name`        varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-    `type`        varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `perm_type`   varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
     `method`      varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
     `path`        text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
     `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-    `group`       text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+    `perm_group`  text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
     `created_at`  timestamp    NULL                                            DEFAULT CURRENT_TIMESTAMP,
     `updated_at`  timestamp    NULL                                            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at`  timestamp    NULL                                            DEFAULT NULL,
